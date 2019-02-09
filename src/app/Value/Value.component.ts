42fd5435e17555ced9178bacd1940e3f6592011a
ahmed jamal <a7medjamal@outlook.com>
2019-02-09 09:52:56 +0300

    016 Making HTTP requests in Angular

---
+++ src/app/Value/Value.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+// tslint:disable-next-line: component-selector
+  selector: 'app-Value',
+  templateUrl: './Value.component.html',
+  styleUrls: ['./Value.component.css']
+})
+export class ValueComponent implements OnInit {
+values: any;
+// tslint:disable-next-line: deprecation
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues() {
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {console.log(response)});
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// tslint:disable-next-line: import-spacing
+import{HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './Value/Value.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+// tslint:disable-next-line: deprecation
+      HttpModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
