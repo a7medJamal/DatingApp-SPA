4dfc3c769a83867035cfb32e952378f3cfbd8662
ahmed jamal <a7medjamal@outlook.com>
2019-02-10 00:28:42 +0300

    016 Making HTTP requests in Angular

--- src/app/Value/Value.component.ts
+++ src/app/Value/Value.component.ts
@@ -17,6 +17,8 @@ values: any;
   }
 
   getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(response => {console.log(response)});
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      console.log(response);
+     });
   }
 }
