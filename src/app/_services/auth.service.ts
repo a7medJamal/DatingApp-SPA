763911bae191f1222bfe3458c1e3fcfa52a119e2
ahmed jamal <a7medjamal@outlook.com>
2019-02-12 11:10:57 +0300

    045 Adding the register method to the Auth Service

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -12,16 +12,22 @@ constructor(private http: Http) { }
 
 login(model: any) {
 // tslint:disable-next-line: deprecation
-    const headers = new Headers({'Content-type': 'application/json'});
-// tslint:disable-next-line: deprecation
-    const options = new RequestOptions({headers: headers});
-// tslint:disable-next-line: deprecation
-return this.http.post(this.baseUrl + 'login' , model, options).map((response: Response) => {
+return this.http.post(this.baseUrl + 'login' , model, this.requestOptions()).map((response: Response) => {
    const user = response.json();
     if (user) {
         localStorage.setItem('token', user.tokenString);
         this.userToken = user.tokenString;
     }
 });
+}
+register(model: any) {
+return this.http.post(this.baseUrl + 'register' , model, this.requestOptions());
+}
+
+private requestOptions() {
+// tslint:disable-next-line: deprecation
+  const headers = new Headers({'Content-type': 'application/json'});
+// tslint:disable-next-line: deprecation
+  return new RequestOptions({headers: headers});
 }
 }
