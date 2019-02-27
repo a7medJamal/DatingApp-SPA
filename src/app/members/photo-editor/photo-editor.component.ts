db1ea1999e57ea25c07fc80b9efcf5e4ab647a05
ahmed jamal <a7medjamal@outlook.com>
2019-02-27 13:58:43 +0300

    112 Using Underscore to manipulate arrays in the SPA

--- src/app/members/photo-editor/photo-editor.component.ts
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -5,6 +5,7 @@ import { environment } from '../../../environments/environment';
 import { AuthService } from '../../_services/auth.service';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-photo-editor',
@@ -16,6 +17,7 @@ export class PhotoEditorComponent implements OnInit {
 uploader: FileUploader;
 hasBaseDropZoneOver = false;
 baseUrl = environment.apiUrl;
+currentMain: Photo;
 
   constructor(private authService: AuthService,
     private userService: UserService,
@@ -57,7 +59,9 @@ baseUrl = environment.apiUrl;
 
  setMainPhoto(photo: Photo) {
   this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
-  console.log('successfully set to main');
+  this.currentMain = _.findWhere(this.photos, {isMain: true});
+  this.currentMain.isMain = true;
+  photo.isMain = true;
   }, error => {
   this.alertify.error(error);
   });
